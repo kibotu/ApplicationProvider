a30b9da1449a142bfb2d85c6bde3154cf2fa3510
Jan Rabe <cloudgazer3d@gmail.com>
2024-12-12 15:52:19 +0100

    ksp

--- app/src/main/java/com/github/florent37/applicationprovider/dagger/AppComponent.kt
+++ app/src/main/java/com/github/florent37/applicationprovider/dagger/AppComponent.kt
@@ -1,11 +1,22 @@
 package com.github.florent37.applicationprovider.dagger
 
+import android.app.Application
 import com.github.florent37.applicationprovider.MainActivity
+import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
-@Component(modules = [AppModule::class])
 @Singleton
+@Component(
+    modules = [
+        AppModule::class
+    ]
+)
 interface AppComponent {
     fun inject(mainActivity: MainActivity)
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance application: Application): AppComponent
+    }
 }
--- app/src/main/java/com/github/florent37/applicationprovider/dagger/DaggerInitializer.kt
+++ app/src/main/java/com/github/florent37/applicationprovider/dagger/DaggerInitializer.kt
@@ -2,12 +2,12 @@ package com.github.florent37.applicationprovider.dagger
 
 import com.github.florent37.application.provider.ApplicationProvider
 
-lateinit var appComponent : AppComponent
+lateinit var graph: AppComponent
 
 val InitializeDagger by lazy {
     ApplicationProvider.listen { application ->
-        appComponent = DaggerAppComponent.builder()
-            .appModule(AppModule(application))
-            .build()
+        graph = DaggerAppComponent
+            .factory()
+            .create(application)
     }
 }
